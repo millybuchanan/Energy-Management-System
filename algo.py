cdf8c767b9aabff561f9ea64bc8a680a494d7170
huyunhao38786 <90305608+edd38786@users.noreply.github.com>
2023-02-20 12:23:41 -0500

    Update algo.py
    Include user schedule as predictive variable

--- algo.py
+++ algo.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-# Load original data into a pandas dataframe
+# Load data into a pandas dataframe
 df = pd.read_csv('energy_consumption.csv')
 
 # Split data into training and testing sets
@@ -11,41 +11,46 @@ train_data = df.iloc[:int(len(df)*0.8)]
 test_data = df.iloc[int(len(df)*0.8):]
 
 # Define feature and target variables
-train_features = train_data[['temperature', 'humidity', 'occupancy', 'fan_speed', 'light_intensity']].values
+train_features = train_data[['temperature', 'humidity', 'occupancy', 'fan_speed', 'light_intensity']]
+
+# Create a new dataframe to store the one-hot encoded schedule
+train_schedule = pd.DataFrame(index=train_data.index, columns=['day_0', 'day_1', 'day_2', 'day_3', 'day_4', 'day_5', 'day_6',
+                                                              'hour_0', 'hour_1', 'hour_2', 'hour_3', 'hour_4', 'hour_5', 'hour_6',
+                                                              'hour_7', 'hour_8', 'hour_9', 'hour_10', 'hour_11', 'hour_12', 'hour_13',
+                                                              'hour_14', 'hour_15', 'hour_16', 'hour_17', 'hour_18', 'hour_19', 'hour_20',
+                                                              'hour_21', 'hour_22', 'hour_23'])
+
+# Fill in the one-hot encoded schedule using the 'schedule' column in the training data
+for i, row in train_data.iterrows():
+    day_index = row['schedule_day']
+    hour_index = row['schedule_hour']
+    train_schedule.loc[i, f'day_{day_index}'] = 1
+    train_schedule.loc[i, f'hour_{hour_index}'] = 1
+
+# Concatenate the one-hot encoded schedule with the other predictor variables
+train_features = pd.concat([train_features, train_schedule], axis=1)
+
 train_target = train_data['energy_consumption'].values
 
 # Create a linear regression model and train it on the training data
 lr_model = LinearRegression()
 lr_model.fit(train_features, train_target)
 
-# Use the trained model to make predictions on the test data
-test_features = test_data[['temperature', 'humidity', 'occupancy', 'fan_speed', 'light_intensity']].values
+# Create the one-hot encoded schedule for the test data
+test_schedule = pd.DataFrame(index=test_data.index, columns=['day_0', 'day_1', 'day_2', 'day_3', 'day_4', 'day_5', 'day_6',
+                                                             'hour_0', 'hour_1', 'hour_2', 'hour_3', 'hour_4', 'hour_5', 'hour_6',
+                                                             'hour_7', 'hour_8', 'hour_9', 'hour_10', 'hour_11', 'hour_12', 'hour_13',
+                                                             'hour_14', 'hour_15', 'hour_16', 'hour_17', 'hour_18', 'hour_19', 'hour_20',
+                                                             'hour_21', 'hour_22', 'hour_23'])
+for i, row in test_data.iterrows():
+    day_index = row['schedule_day']
+    hour_index = row['schedule_hour']
+    test_schedule.loc[i, f'day_{day_index}'] = 1
+    test_schedule.loc[i, f'hour_{hour_index}'] = 1
+
+# Concatenate the one-hot encoded schedule with the other predictor variables
+test_features = pd.concat([test_data[['temperature', 'humidity', 'occupancy', 'fan_speed', 'light_intensity']], test_schedule], axis=1)
+
 test_predictions = lr_model.predict(test_features)
 
-# Evaluate the performance of the model on the test data using mean squared error
 mse = np.mean((test_predictions - test_data['energy_consumption'].values)**2)
-
-# Print the mean squared error
-print('Mean Squared Error: ', mse)
-
-# Load new data into a pandas dataframe
-new_data = pd.read_csv('new_energy_consumption.csv')
-
-# Add new data to the original training dataset
-updated_train_data = pd.concat([train_data, new_data])
-
-# Define feature and target variables for the updated training data
-updated_train_features = updated_train_data[['temperature', 'humidity', 'occupancy', 'fan_speed', 'light_intensity']].values
-updated_train_target = updated_train_data['energy_consumption'].values
-
-# Retrain the model on the updated training data
-lr_model.fit(updated_train_features, updated_train_target)
-
-# Use the trained model to make predictions on the test data
-updated_test_predictions = lr_model.predict(test_features)
-
-# Evaluate the performance of the updated model on the test data using mean squared error
-updated_mse = np.mean((updated_test_predictions - test_data['energy_consumption'].values)**2)
-
-# Print the updated mean squared error
-print('Updated Mean Squared Error: ', updated_mse)
