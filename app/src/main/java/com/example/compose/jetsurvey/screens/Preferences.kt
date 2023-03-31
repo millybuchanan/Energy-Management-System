1323f5ad8ec8e6564796ed7d67015ec010d7c935
huyunhao38786 <90305608+edd38786@users.noreply.github.com>
2023-03-31 08:04:05 -0400

    Update Preferences.kt

--- app/src/main/java/com/example/compose/jetsurvey/screens/Preferences.kt
+++ app/src/main/java/com/example/compose/jetsurvey/screens/Preferences.kt
@@ -2,14 +2,11 @@ package com.example.compose.jetsurvey.screens
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,6 +18,12 @@ import com.example.compose.jetsurvey.theme.JetsurveyTheme
 @Preview(name = "Light", uiMode = Configuration.UI_MODE_NIGHT_NO)
 fun PreferencesScreen() {
     Column {
+        Text(
+            text = "Preferences",
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.onBackground,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
 
         JetsurveyTheme() {
             Surface {
