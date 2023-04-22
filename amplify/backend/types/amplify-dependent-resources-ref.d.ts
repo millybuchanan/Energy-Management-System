13886be632c9caebdbfc6ac10380dafde3397209
millybuchanan <124624000+millybuchanan@users.noreply.github.com>
2023-04-22 14:28:12 -0400

    Add files via upload

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "TestCRUDapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
